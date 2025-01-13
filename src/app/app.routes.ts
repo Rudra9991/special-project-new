6f3b659ee20bff92d8ceb249e928b29d9ce1cb18
Rudra9991 <rurasish.das0@gmail.com>
2025-01-14 01:35:37 +0530

    first checking

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NextPageComponent } from './next-page/next-page.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '', redirectTo:'/firstPage',pathMatch: 'full'},
+    {path: 'NextPage', component: NextPageComponent},
+    {path: 'firstPage', component: FirstPageComponent}
+];
---
+++ src/app/next-page/next-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-next-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './next-page.component.html',
+  styleUrl: './next-page.component.css'
+})
+export class NextPageComponent {
+
+}
